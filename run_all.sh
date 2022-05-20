f9c9f1614cd107ce512d89ac5e39e64a6f3cb61e
Scott M Stark <starksm64@gmail.com>
2022-05-20 01:39:58 -0500

    Update run_all.sh to echo tck, add core-tck
    Signed-off-by: Scott M Stark <starksm64@gmail.com>

--- run_all.sh
+++ run_all.sh
@@ -2,25 +2,37 @@
 
 # Sample run script to go through the steps of running the TCKs once installed
 
+# Uncomment if running against staged API artifacts
+#STAGING=-Pstaging
+
+echo "Running jsonp-standalone-tck"
 cd jsonp-standalone-tck
-mvn -Pstaging verify
+mvn ${STAGING} verify
 cd ..
 
+echo "Running jsonb-standalone-tck"
 cd jsonb-standalone-tck
-mvn -Pstaging verify
+mvn ${STAGING} verify
 cd ..
 
+echo "Running rest-tck"
 cd rest-tck
-mvn -Pstaging -Pupdate-wildfly validate
-mvn -Pstaging verify
+mvn ${STAGING} -Pupdate-wildfly validate
+mvn ${STAGING} verify
 cd ..
 
+echo "Running inject-tck"
 cd inject-tck
-mvn -Pstaging verify
+mvn ${STAGING} verify
 cd ..
 
+echo "Running cdi-tck"
 cd cdi-tck
-mvn -Pstaging -Pupdate-wildfly validate
-mvn -Pstaging verify
+mvn ${STAGING} -Pupdate-wildfly validate
+mvn ${STAGING} verify
 cd ..
 
+echo "Running core-tck"
+cd core-tck
+mvn ${STAGING} verify
+cd ..
