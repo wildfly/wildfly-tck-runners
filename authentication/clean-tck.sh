18027041e1ffd65bfabb3f35c5d8e1c2b961d771
Darran Lofthouse <darran.lofthouse@jboss.com>
2022-07-06 20:09:37 +0100

    Remove unused environment variables.

--- authentication/clean-tck.sh
+++ authentication/clean-tck.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
 
-TCK_URL=https://download.eclipse.org/ee4j/jakartaee-tck/jakartaee10/staged/eftl/jakarta-authentication-tck-3.0.1.zip
 TCK_ZIP=jakarta-authentication-tck-3.0.1.zip
 TCK_HOME=authentication-tck-3.0.1
-TCK_ROOT=$TCK_HOME/tck
-WILDFLY_HOME=wildfly/target/wildfly
 
 rm -fR $TCK_HOME
 rm $TCK_ZIP
